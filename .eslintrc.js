20220377f9a985323acc062eca037312eaa4b9fb
piotr.kaczmarek <piotr.kaczmarek@optad360.com>
2022-05-21 22:34:52 +0200

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "simple-import-sort"],
+  rules: {
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "react/prop-types": "off",
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import "./index.scss";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.scss";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
