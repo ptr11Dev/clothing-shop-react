b706b9944be12d21585511bcbe3356b26b34c929
ptr11dev <piotrkaczmarek.dev@gmail.com>
2022-05-21 22:34:52 +0200

    Add eslint

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Homepage from "./routes/Homepage/Homepage";
 import Navigation from "./routes/Navigation/Navigation";
--- src/routes/Navigation/Navigation.jsx
+++ src/routes/Navigation/Navigation.jsx
@@ -1,5 +1,7 @@
 import "./Navigation.scss";
-import { Outlet, Link } from "react-router-dom";
+
+import { Link, Outlet } from "react-router-dom";
+
 import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
 
 const Navigation = () => {
