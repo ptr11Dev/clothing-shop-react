59067ae0f6ac7a865fdc01286054b97a2c7b8c74
piotr.kaczmarek <piotr.kaczmarek@optad360.com>
2022-05-21 13:46:08 +0200

    Add firebase

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 
 import Homepage from "./routes/Homepage/Homepage";
 import Navigation from "./routes/Navigation/Navigation";
+import SignIn from "./routes/SingIn/SignIn";
 
 const Shop = () => {
   return <h1>I am the shop page</h1>;
@@ -13,6 +14,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Homepage />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
