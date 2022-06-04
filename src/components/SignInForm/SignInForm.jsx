4a9969d481de4d2585cc5e85441a1457ef170508
piotr.kaczmarek <piotr.kaczmarek@optad360.com>
2022-06-04 11:49:22 +0200

    Use auth stream from firebase lib

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -24,16 +24,14 @@ const SignInForm = () => {
   };
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
-    await createUserDocumentFromAuth(user);
+    await signInWithGooglePopup();
   };
 
   const handleSumbit = async (e) => {
     e.preventDefault();
 
     try {
-      const response = await signInUserWithEmailAndPassword(email, password);
-      console.log(response);
+      const { user } = await signInUserWithEmailAndPassword(email, password);
       resetFormFields();
     } catch (err) {
       switch (err.code) {
--- src/routes/Navigation/Navigation.jsx
+++ src/routes/Navigation/Navigation.jsx
@@ -1,10 +1,15 @@
 import "./Navigation.scss";
 
+import { useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 import { ReactComponent as CrownLogo } from "../../assets/crown.svg";
+import { UserContext } from "../../contexts/User";
+import { signOutUser } from "../../utils/firebase/firebase";
 
 const Navigation = () => {
+  const { currentUser } = useContext(UserContext);
+
   return (
     <>
       <div className="navigation">
@@ -15,9 +20,15 @@ const Navigation = () => {
           <Link className="navLink" to="/shop">
             SHOP
           </Link>
-          <Link className="navLink" to="/auth">
-            SIGN IN
-          </Link>
+          {currentUser ? (
+            <span className="navLink" onClick={signOutUser}>
+              SIGN OUT
+            </span>
+          ) : (
+            <Link className="navLink" to="/auth">
+              SIGN IN
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
